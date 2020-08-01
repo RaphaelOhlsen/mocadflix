b3ac5d41bc093b47fcc2de72ff52352b4917c921
Raphael Ohlsen <raphael.ohlsen@gmail.com>
2020-08-01 08:57:35 -0300

    Efeito de botão progressivo esquerda-direita

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Header, { Wrapper } from './components/Header';
 import Logo from './components/Logo';
 import Button from './components/Button';
 import Footer from './components/Footer';
-import Banner, {TextContainer} from './components/Banner';
+import Banner, { TextContainer } from './components/Banner';
 import Link from './components/Link';
 import Tag from './components/Tag';
 import Title from './components/Title';
@@ -17,7 +17,7 @@ import Description from './components/Description';
 import Thumbnail from './components/Thumbnail';
 
 import maujor from './assets/img/maujor.png';
-import avatar from './assets/img/avatar.png'
+import avatar from './assets/img/avatar.png';
 
 function App() {
   return (
@@ -25,7 +25,9 @@ function App() {
       <Header>
         <Wrapper>
           <Logo />
-          <Button as="a" href="/video/new">Novo Vídeo</Button>
+          <Button as="a" href="/video/new">
+            Novo vídeo
+          </Button>
         </Wrapper>
       </Header>
       <Banner>
@@ -33,24 +35,28 @@ function App() {
           <Tag> Front End</Tag>
           <Title>MAUJOR - O Dinossauro das CSS </Title>
           <Description>
-            Não é uma simples entrevista, mas sim um grande motivador para todos 
-            que querem iniciar na área de desenvolvimento. Ouvimos tantas 
-            histórias legais que decidimos fazer uma série de vídeos com esse bate
-            papo tão incrível que tivemos com o Maujor® - O dinossauro das CSS!!!
+            Não é uma simples entrevista, mas sim um grande motivador para todos
+            que querem iniciar na área de desenvolvimento. Ouvimos tantas
+            histórias legais que decidimos fazer uma série de vídeos com esse
+            bate papo tão incrível que tivemos com o Maujor® - O dinossauro das
+            CSS!!!
           </Description>
         </TextContainer>
-        <Thumbnail 
-          src={maujor} 
+        <Thumbnail
+          src={maujor}
           alt="Thumbnail do Maujor"
           title="MAUJOR - O Dinossauro das CSS"
           avatar={avatar}
           channelName="CDF TV"
         />
       </Banner>
-      <Footer >
+      <Footer>
         <Logo />
         <p>
-          Site feito na <Link href="https://alura.com.br"><LogoImersao /></Link>
+          Site feito na{' '}
+          <Link href="https://alura.com.br">
+            <LogoImersao />
+          </Link>
         </p>
       </Footer>
     </>
--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,26 +1,34 @@
 import styled from 'styled-components';
 
 const Button = styled.button`
-  background: var(--dark);
+  position: relative;
+  padding: 1.2rem 3rem;
+  color: var(--grayLight);
   border: 1px solid var(--grayLight);
   box-sizing: border-box;
-  color: var(--grayLight);
-  cursor: pointer;
   font-size: 1.6rem;
   font-style: normal;
   font-weight: 600;
-  padding: 1.2rem 3rem;
+  cursor: pointer;
   transition: background-color 200ms linear,
     transform 200ms cubic-bezier(0, 0, 0.73, 2.24);
 
-  &:hover {
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 0;
+    height: 100%;
     background-color: var(--border);
-    transform: scale(1.05);
+    z-index: -1;
   }
 
-  &:active {
-    transition: background-color 200ms linear, transform 100ms linear;
-    transform: scale(0.95);
+  &:hover {
+    &::before {
+      width: 100%;
+      transition: width 250ms linear;
+    }
   }
 `;
 
